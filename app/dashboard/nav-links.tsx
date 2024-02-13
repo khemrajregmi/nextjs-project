ad4286d7d9e53afb05005191884b5da1c373eeea
khemraj regmi <khemrajregmi@khemrajs-mbp.localdomain>
2024-02-13 13:30:36 +0100

    chapter 5 comitted links part in nav-link

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -16,7 +16,7 @@ export default function NavLinks() {
  
   return (
     <>
-      {links.map((link) => {
+      {/* {links.map((link) => {
         const LinkIcon = link.icon;
         return (
           <Link
@@ -33,7 +33,7 @@ export default function NavLinks() {
             <p className="hidden md:block">{link.name}</p>
           </Link>
         );
-      })}
+      })} */}
     </>
   );
 }
