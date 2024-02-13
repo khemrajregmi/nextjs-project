9bbfc9c66cfda517fc3430d31d40cec1f859e863
khemraj regmi <khemrajregmi@khemrajs-mbp.localdomain>
2024-02-13 12:38:42 +0100

    added image tag in next js way

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+
 
 
 
@@ -36,10 +38,13 @@ export default function Page() {
             </Link>
           </div>
           <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-            <img
+            <Image
               src="/hero-desktop.png"
+              width={1000}
+              height={760}
+              className="hidden md:block"
               alt="Screenshots of the dashboard project showing desktop version"
-            />          
+            />         
           </div>
         </div>
       </main></>
