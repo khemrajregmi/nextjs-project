f4cb1f70a7f6343d1704f2e74d7f830f3f820389
khemraj regmi <khemrajregmi@khemrajs-mbp.localdomain>
2024-02-16 16:05:55 +0100

    chapter 12 deleting an invoice

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -16,8 +16,8 @@ export function CreateInvoice() {
 export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
-      href="/dashboard/invoices"
-      className="rounded-md border p-2 hover:bg-gray-100"
+    href={`/dashboard/invoices/${id}/edit`}
+    className="rounded-md border p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
     </Link>
