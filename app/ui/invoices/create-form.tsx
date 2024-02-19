cc0e9e1235631a97e8147adcfc3e4c3409928eda
khemraj regmi <khemrajregmi@khemrajs-mbp.localdomain>
2024-02-19 12:08:08 +0100

    chapter 14 improving accessibility in edit and add invoice form

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -18,7 +18,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 
   const [state, dispatch] = useFormState(createInvoice, initialState);
 
-  <form action={dispatch}>
+  return <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -68,6 +68,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                required
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
