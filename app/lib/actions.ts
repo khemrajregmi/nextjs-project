4fc33228fb11680ff793fb4449d28f7e5c255ba1
khemraj regmi <khemrajregmi@khemrajs-mbp.localdomain>
2024-02-21 12:44:49 +0100

    minor change

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,7 +4,7 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-// import { signIn } from '@/auth';
+//import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
 
  
